75deb11ec286e028854d6fb8aa4c54dacc86fccd
Garrett <garrett56x@gmail.com>
2020-10-22 16:35:20 -0700

    Using card component for pois

--- src/client/Components/ItemCard/ItemCard.jsx
+++ src/client/Components/ItemCard/ItemCard.jsx
@@ -1,30 +1,57 @@
-import React from "react";
-import "./ItemCard.css";
-import { Paper } from "@material-ui/core";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Card, CardHeader, CardMedia, CardContent, CardActions, IconButton, Typography } from '@material-ui/core';
+import FavoriteIcon from '@material-ui/icons/Favorite';
 
-class ItemCard extends React.Component {
-  render() {
-    const { item } = this.props;
-    const images = require.context('../../../assets', true);
-	  const img = images('./' + item.image);
-
-    return (
-      <Paper className="item-card">
-        <div className="item-image" style={{ backgroundImage: `url(${img})` }}>
-        </div>
-        <div className="item-info">
-          <h3>{item.name}</h3>
-          <p className="contact">
-          	<a target="_blank" rel="noopener noreferrer" href={item.website}>Website</a> | <a href={`tel:${item.phone}`}>{item.phone}</a>
-          </p>
-          <p>{item.location}</p>
-          <p><span className="info-title">His Favorite: </span>{item.his}</p>
-          <p><span className="info-title">Her Favorite: </span>{item.hers}</p>
-          <p><span className="info-title">We Recommend: </span>{item.recommendation}</p>
-        </div>
-      </Paper>
-    );
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  contentSection: {
+    fontWeight: "bold",
   }
-}
+}));
 
-export default ItemCard;
+export default function ItemCard({item}) {
+  const classes = useStyles();
+  const images = require.context('../../../assets', true);
+	const img = images('./' + item.image);
+
+  return (
+    <Card className={classes.root}>
+      <CardHeader
+        action={
+          <IconButton aria-label="add to favorites">
+            <FavoriteIcon />
+          </IconButton>
+        }
+        title={item.name}
+      />
+      <CardMedia
+        className={classes.media}
+        image={img}
+        title={item.name}
+      />
+      <CardContent>
+        <Typography variant="body2">
+          <span className={classes.contentSection}>Her Favorite:</span> {item.hers}
+        </Typography>
+        <Typography variant="body2">
+          <span className={classes.contentSection}>His Favorite:</span> {item.his}
+        </Typography>
+        <Typography variant="body2">
+          <span className={classes.contentSection}>Recommendation:</span> {item.recommendation}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        {/* <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton> */}
+      </CardActions>
+    </Card>
+  );
+}
