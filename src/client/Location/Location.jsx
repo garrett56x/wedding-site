ac15fc7f80b9d3eab9fc8f753d4c8ca5746f7c39
Garrett Van Neste <garrettvanneste@Garretts-MacBook-Pro.local>
2021-08-10 20:31:45 -0700

    Updated time

--- src/client/Location/Location.jsx
+++ src/client/Location/Location.jsx
@@ -7,29 +7,47 @@ export default function Location() {
   return (
     <div className="location">
       <Hero
-      	backgroundImage={BackgroundImage}
-      	backgroundPosition="top"
-      	phrase="WHEN &amp; WHERE"
+        backgroundImage={BackgroundImage}
+        backgroundPosition="top"
+        phrase="WHEN &amp; WHERE"
       />
       <div className="content">
-      	<h2>Date &amp; Time</h2>
-      	<p>October 2nd, 2021 - 4PM</p>
+        <h2>Date &amp; Time</h2>
+        <p>October 2nd, 2021 - 5PM</p>
 
-		<h2>Dress Code</h2>
-      	<p>Black tie optional</p>
+        <h2>Dress Code</h2>
+        <p>Black tie optional</p>
 
-      	<h2>Location</h2>
-      	<p>Hotel Ballard</p>
-      	<p><a href="tel:206-789-5012">(206) 789-5012</a> | <a target="_blank" rel="noopener noreferrer" href="https://www.hotelballardseattle.com/en-us">Hotel Website</a></p>
-      	<p>
-			We have reserved a small number of rooms for our guests.
-			Please reference the Van Neste / Wang party when making your reservations for a special discount.
-		</p>
-		<div className="gmap_canvas">
-			<iframe title="map" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=hotel%20ballard&t=&z=15&ie=UTF8&iwloc=&output=embed" frameBorder={0} scrolling="no" marginHeight={0} marginWidth={0}>
-			</iframe>
-		</div>
-	  </div>
+        <h2>Location</h2>
+        <p>Hotel Ballard</p>
+        <p>
+          <a href="tel:206-789-5012">(206) 789-5012</a> |{" "}
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.hotelballardseattle.com/en-us"
+          >
+            Hotel Website
+          </a>
+        </p>
+        <p>
+          We have reserved a small number of rooms for our guests. Please
+          reference the Van Neste / Wang party when making your reservations for
+          a special discount.
+        </p>
+        <div className="gmap_canvas">
+          <iframe
+            title="map"
+            height="500"
+            id="gmap_canvas"
+            src="https://maps.google.com/maps?q=hotel%20ballard&t=&z=15&ie=UTF8&iwloc=&output=embed"
+            frameBorder={0}
+            scrolling="no"
+            marginHeight={0}
+            marginWidth={0}
+          ></iframe>
+        </div>
+      </div>
     </div>
   );
 }
