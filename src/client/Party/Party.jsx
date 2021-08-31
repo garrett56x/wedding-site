823433b3c2fbc965a8767bdc74e874c78ff4aa8e
Garrett Van Neste <garrettvanneste@Garretts-MacBook-Pro.local>
2021-08-30 21:39:04 -0700

    Added time to rehersal dinner

--- src/client/Party/Party.jsx
+++ src/client/Party/Party.jsx
@@ -14,13 +14,13 @@ export default function Party() {
         phrase="PARTY WITH US"
       />
       <div className="content">
-        <h2>Rehersal Dinner</h2>
-        <p>Belltown Pizza</p>
+        <h2>Welcome Dinner</h2>
+        <p>Belltown Pizza | October 1st | 6pm</p>
         <p>
           <a href="tel:206-441-2653">(206) 441-2653</a>
         </p>
         <p>
-          We are hosting a rehersal dinner on October 1st at Belltown Pizza, the
+          We are hosting a welcome dinner on October 1st at Belltown Pizza, the
           place where we met! Join us for some pizza, meatballs, and drinks.
         </p>
         <div className="gmap_canvas">
